66e5c8274fcc607a10987d45668be4b1baee6091
MatosRicardo <ricardoribeirodematos@gmail.com>
2025-01-18 12:05:24 -0300

    Projeto em andamento

--- Rickzap.py
+++ Rickzap.py
@@ -2,14 +2,43 @@
 import flet as ft
 
 # Criando função principal para rodar o aplicativo
-def main ():
-  # título
-  titulo = ft.Text("RickZap")
-  pagina.add(titulo)
-  
-  # botão inicial
-  botao = ft.ElevatedButton("Iniciar Chat")
-  pagina.addd(botao)
+def main(page: ft.Page):  # Recebe 'page' como argumento
+    # título
+    titulo = ft.Text("RickZap")
+    
+    def entrar_chat(evento):
+        # Fechar o pop-up
+        popup.open = False
+        # Sumir com o titulo
+        page.remove(titulo)
+        # Sumir com o botao
+        page.remove(botao)
+        
+        
+        
+        
+        pagina.update()
+      
+    
+    # Criando pop-up
+    titulo_popup = ft.Text("Bem-vindo ao RickZap")
+    caixa_nome = ft.TextField(label="Digite seu nome")
+    botao_popup = ft.ElevatedButton("Entrar no Chat", on_click=entrar_chat)
+    popup = ft.AlertDialog(title=titulo_popup, content=caixa_nome, actions=[botao_popup])
+    
+    # Função para abrir o modal
+    def abrir_modal(evento):
+        page.dialog = popup  # Configura o pop-up para a página
+        popup.open = True    # Abre o pop-up
+        page.update()        # Atualiza a página
+        print("Clicou no botão")
+      
+    # Botão inicial
+    botao = ft.ElevatedButton("Iniciar Chat", on_click=abrir_modal)
+      
+    # Colocar os elementos na página
+    page.add(titulo)  # Adiciona o título à página
+    page.add(botao)   # Adiciona o botão à página
   
 # Executando
-ft.app(main)
+ft.app(target=main, view=ft.WEB_BROWSER)
